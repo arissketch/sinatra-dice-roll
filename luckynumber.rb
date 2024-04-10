e52a0ff67c951f7ec9733552cd7d52b368a02889
arissketch <60331491+arissketch@users.noreply.github.com>
2024-04-10 16:16:11 +0000

    save2

--- luckynumber.rb
+++ luckynumber.rb
@@ -1,11 +1,28 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get "/" do
-  lucky_num = rand(100)
-  return "Your lucky number is " + lucky_num.to_s
+require "better_errors"
+require "binding_of_caller"
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+
+
+
+get ("/") do
+
+  erb(:elephant)
 end
 
-get("/giraffe") do
-  "Hopefully this shows up without having to restart the server 🤞🏾"
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
 end
